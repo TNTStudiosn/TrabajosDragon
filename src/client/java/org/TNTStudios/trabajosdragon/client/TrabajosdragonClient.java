5cc96d2691f1f043e26a63bd791d57e3b542341d
TNTStudiosn <tntstudiosn@gmail.com>
2025-01-28 11:42:20 -0600

    Initial commit

---
+++ src/client/java/org/TNTStudios/trabajosdragon/client/TrabajosdragonClient.java
@@ -0,0 +1,10 @@
+package org.TNTStudios.trabajosdragon.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TrabajosdragonClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/TNTStudios/trabajosdragon/Trabajosdragon.java
@@ -0,0 +1,10 @@
+package org.TNTStudios.trabajosdragon;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Trabajosdragon implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
